921d77c6bd9409a9ba73b3c43bb1f4b5ff04532d
Amal Gupta <gupta.am@live.in>
2022-03-07 23:07:19 +0530

    SFF-10: Building The Spring Boot Generic Executor Project

---
+++ spring-boot-generic-executor/src/test/java/com/sff/springbootgenericexecutor/SpringBootGenericExecutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sff.springbootgenericexecutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGenericExecutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
