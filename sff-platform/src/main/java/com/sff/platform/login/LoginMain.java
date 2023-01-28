930b5c111fcda2a5bfe91760edb810a571af2f19
Amal Gupta <gupta.am@live.in>
2023-01-28 23:32:46 +0530

    SFF-30: First Commit for Platform Code

---
+++ sff-platform/src/main/java/com/sff/platform/login/LoginMain.java
@@ -0,0 +1,18 @@
+package com.sff.platform.login;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginMain implements CommandLineRunner {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
